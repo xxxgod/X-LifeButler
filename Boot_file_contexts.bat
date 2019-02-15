bebd71973ad0c4518d0ffb3a6121cb4c036a5d16
X神之怒 <939290261@qq.com>
2019-02-15 11:04:15 +0800

    Add files via upload

---
+++ Boot_file_contexts.bat
@@ -0,0 +1,41 @@
+ÿş
+::  file_contextsÌáÈ¡  by @xiaohuzz    2015.11.15
+@echo off
+%~d0
+cd "%~p0"
+IF EXIST "%~dp0windows_tools" SET PATH=%PATH%;"%~dp0windows_tools"
+chmod -R 755 "%~dp0windows_tools"
+Setlocal EnableDelayedExpansion
+mode con lines=30 cols=70
+
+if "%~1" == "" goto noargs
+if /i "%~x1" NEQ ".img" goto noargs
+set "file=%~f1"
+if exist "%~n1" rd /s /q "%~n1" >nul
+mkdir "%~n1"
+copy "%file%" "%~n1" >nul
+cd "%~n1"
+ren "%~nx1" boot.img
+bootimg.exe --unpack-bootimg boot.img
+cd ..
+copy "%~n1\initrd\file_contexts" "system_temp" >nul
+echo.
+echo   Ok  Íê³É£¡
+echo.
+rd /s /q "%~n1" >nul
+ping 127.1 -n 2 >nul 
+goto end
+
+:noargs
+echo.
+echo Çë°Ñ boot.img ÍÏÖÁ Boot_file_contexts.bat
+
+:error
+echo.
+echo ´íÎó£¡
+echo.
+echo ÈÎÒâ¼ü¹Ø±Õ´°¿Ú
+echo.
+pause >nul
+
+:end
---
+++ Start.bat
@@ -0,0 +1,21 @@
+ÿþ
+@echo off
+cls
+cd "%~dp0"
+IF EXIST "%~dp0windows_tools" SET PATH=%PATH%;"%~dp0windows_tools"
+chmod -R 755 "%~dp0windows_tools"
+Setlocal EnableDelayedExpansion
+mode con lines=30 cols=70
+
+title Android RomÖúÊÖ-Õ¬¿Æ¼¼-¼«¿ÍÂÛÌ³ bbs.zecoki.com
+ctext "{0A}»·¾³ÅäÖÃÖÐ......{07} "
+ping 127.0.0.1 -n 3 2>nul >nul
+echo.
+ctext "{0A}»·¾³¼ÓÔØÍê³É£¡{07} "
+curl -O http://www.zecoki.com/xtools/rom-tools/readme.txt 2>nul 
+curl -O http://www.zecoki.com/xtools/rom-tools/Start.bat 2>nul 
+curl -O http://www.zecoki.com/xtools/rom-tools/windows_tools/temp/xtools.bat 2>nul 
+copy xtools.bat windows_tools\temp\xtools.bat
+call windows_tools\temp\xtools.bat
+exit
+
